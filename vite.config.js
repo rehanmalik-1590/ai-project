50eaba9dd4b953e000f766fcd9397d3f98ad034c
rehanmalik-1590 <rehanmalikiubroll21@gmail.com>
2025-06-17 13:50:01 +0500

    fifth commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/ai-project/', // Make sure this matches your GitHub repo name
-  plugins: [react()]
+  plugins: [react(),
+    tailwindcss()
+  ]
 });
